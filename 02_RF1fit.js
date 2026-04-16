32108ff9824d9c7a8930422af2d26fb32e783d42
lican.e.martinez <lican.e.martinez@gmail.com>
2026-04-16 13:29:32 +0000

    Updated 02_RF1fit.js (added 2 lines, deleted 2 lines).

--- 02_RF1fit.js
+++ 02_RF1fit.js
@@ -71,7 +71,7 @@ var samples_rf1 = mergedCollection.map(function(image) {
 
 }).flatten();
 
-
+print(samples_rf1.size());
 
 // =============================================================================
 // 4. TRAIN RF1
@@ -107,7 +107,7 @@ var predictRF1 = function(image) {
 
   // return predClean.set('year', image.get('year'));
   
-  return pred
+  return pred.set('year', image.get('year'));
   
 };
 
