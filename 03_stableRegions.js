488dfd9a26b048e3c6aad839d720b3ae687cc467
lican.e.martinez <lican.e.martinez@gmail.com>
2026-04-20 19:27:32 +0000

    Updated 03_stableRegions.js (added 1 line, deleted 1 line).

--- 03_stableRegions.js
+++ 03_stableRegions.js
@@ -91,7 +91,7 @@ if (EXPORT_TO_DRIVE) {
     collection    : histFC,
     description   : '03_SumOnes_Histogram_rf1patchFiltered',
     folder        : DRIVE_FOLDER,
-    fileNamePrefix: '03_SumOnes_Histogram_rf1patchFiltered',
+    fileNamePrefix: '03_SumOnes_Histogram_rf1patchFiltered_compCtrl',
     fileFormat    : 'CSV'
   });
 }
